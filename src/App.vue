5b35ce8a88ae40652764f7b36980fe8192810da3
DJamMix <antropov.viktor1234@yandex.ru>
2022-12-13 14:01:24 +0400

    Сверстана навигация сайта

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <nav class="nav">
     <div class="nav_blocklogo">
-      <a href="#"><img src="./assets/img/logo.svg" alt="Логотип"></a>
+      <router-link to="/"><img src="./assets/img/logo.svg" alt="Логотип"></router-link>
     </div>
     <div class="nav_block-menu">
-      <a class="nav_block-menu_item" href="#">Главная</a>
-      <a class="nav_block-menu_item" href="#">Правила</a>
+      <router-link to="/" class="nav_block-menu_item">Главная</router-link>
+      <router-link to="/about" class="nav_block-menu_item">Правила</router-link>
     </div>
-    <div>
-      <a href="#"><img src="./assets/img/icon-vk.svg" alt="Перейти в группу Вконтакте"></a>
-      <a href="#"><img src="./assets/img/icon-youtube.svg" alt="Перейти на канал Ютуб"></a>
-      <a href="#"><img src="./assets/img/icon-discord.svg" alt="Перейти в Discord"></a>
+    <div class="nav_social-block">
+      <a class="nav_social-block_item" href="#"><img src="./assets/img/icon-vk.svg" alt="Перейти в группу Вконтакте"></a>
+      <a class="nav_social-block_item" href="#"><img src="./assets/img/icon-youtube.svg" alt="Перейти на канал Ютуб"></a>
+      <a class="nav_social-block_item" href="#"><img src="./assets/img/icon-discord.svg" alt="Перейти в Discord"></a>
     </div>
-    <div>
-      <a href="#"><img src="./assets/img/icon-lk.svg" alt="Личный кабинет"><p>Личный кабинет</p></a>
+    <div class="nav_pers-account-block">
+      <a class="nav_pers-account-block_link" href="#">
+        <img class="nav_pers-account-block_link_img" src="./assets/img/icon-lk.svg" alt="Личный кабинет">
+        <p class="nav_pers-account-block_link_content">Личный кабинет</p>
+      </a>
     </div>
   </nav>
   <router-view/>
