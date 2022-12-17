afbe1fb93278c4849c0d27664c432871a0a37550
wkyren <apetrenko1402@gmail.com>
2022-12-17 15:43:30 +0300

    Обновление файлов

--- src/components/Rules/RulesComponent.vue
+++ src/components/Rules/RulesComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="rules">
         <div class="rules_rules-block">
-            <div class="rules_rules-block_text">
-                <h2>ПРАВИЛА ПРОЕКТА</h2>
-                <div></div>
+            <div class="rules_rules-block_object">
+                <h2>ПРАВИЛА ПРОЕКТА<div></div></h2>
+                
             </div>
             <List />
         </div>
@@ -26,22 +26,23 @@ export default {
             padding-bottom: 67px
             background: #F3F3F3
             &_rules-block
-                max-width: 1170px
                 margin: auto 
-                &_text
-                    position: relative
+                max-width: 1170px
+                &_object
                     padding-bottom: 62px
+                    position: relative
+                    justify-content: center
+                    align-items: center
+                    text-align: center
                     h2
                         font-size: 27px
                         font-family: 'Montserrat-extrabold'
                         font-weight: 800
                         text-align: center
-                    div
-                        background: linear-gradient(309deg, rgba(251,176,52,1) 0%, rgba(255,221,0,1) 100%)
-                        width: 58px
-                        height: 16px
-                        margin-left: 680px
-                        margin-top: -33px
-
-                
+                        div
+                            background: linear-gradient(309deg, rgba(251,176,52,1) 0%, rgba(255,221,0,1) 100%)
+                            width: 58px
+                            margin-left: 59%
+                            margin-top: -3%
+                            height: 16px
 </style>
